5640f8cf40fe9ae6aded24e910c55ac8d3dc5107
jalal1985khan <jalal1985khan@gmail.com>
2023-01-06 13:26:27 +0530

    uninstalled next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
 }
 
 module.exports = nextConfig
-
-const webpack = require("webpack");
-
-webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  config.plugins.push(
-   new webpack.ProvidePlugin({
-   $: "jquery",
-   jQuery: "jquery",
-   "window.jQuery": "jquery",
-}));
-}
-return config;
-
 
 
 
