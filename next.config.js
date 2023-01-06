e33354a3fb83db6e2647d3d72310a258a770c6b6
jalal1985khan <jalal1985khan@gmail.com>
2023-01-06 13:28:47 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
 module.exports = nextConfig
 
 
+// const webpack = require("webpack");
 
+// webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//   config.plugins.push(
+//    new webpack.ProvidePlugin({
+//    $: "jquery",
+//    jQuery: "jquery",
+//    "window.jQuery": "jquery",
+// }));
+// }
+// return config;
 
 
